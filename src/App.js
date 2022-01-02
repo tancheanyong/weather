14433b63a73be9578f8146afc55781b4b33651e5
Ben <tancheanyong@gmail.com>
2022-01-02 16:06:03 +0800

    time now shows local city time

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useState,useEffect } from 'react';
 import { FaSearch } from "react-icons/fa";
+
 const api = {
   key: "73916e94274abc0225c6d6e3c8b6a997",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 function App() {
   const [query, setQuery] = useState('');
-  const [weather,setWeather] =useState('');
-  const [clock,setClock]=useState();
+  const [weather,setWeather] =useState([]);
 
   const search = (e) =>{
     e.preventDefault();
@@ -20,21 +20,18 @@ function App() {
       });
     
   }
-
-  useEffect(()=>{
-    let t = new Date();
-    setClock(t.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}));
-    setInterval(() => {
-      let t = new Date();
-      setClock(t.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}));
-    }, 60000);
-  },[])
+  const timeBuilder = () =>{
+    let t= new Date();
+    let time=new Date(t.getTime()+(t.getTimezoneOffset()*60000)+(weather.timezone*1000));
+    console.log(time.getHours());
+    return time.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+  }
     
 
   const dateBuilder = (d) =>{
     const months=["January","Febuary","March","April","May","June","July","August","September","October","November","December"];
     const days=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-
+    
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
@@ -66,7 +63,7 @@ function App() {
             <div className="locationBox">
               <div className="location">{weather.name},{weather.sys.country}</div>
               <div className="date">{dateBuilder(new Date())}</div>
-              <div className="time">{clock}</div>
+              <div className="time">{timeBuilder()}</div>
             </div>
             <div className="weatherBox">
               <div className="temp">{Math.round(weather.main.temp)}&deg;C</div>
